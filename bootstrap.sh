adb88ac98212de1ddd68aaf5b0c18b4d1e0e69de
lgabster <lgabster@gmail.com>
2016-10-22 22:18:28 +0200

    add sublime settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ git config --global user.name "lgabster"
 git config --global user.email "lgabster@gmail.com"
 git config --global credential.helper osxkeychain
 git config --global mergetool.sublime.cmd "subl -w \$MERGED"
-git config --global mergetool.sublime.trustExitCode false  
+git config --global mergetool.sublime.trustExitCode false
 git config --global merge.tool sublime
 git mergetool -y
 
@@ -35,36 +35,36 @@ git pull origin master;
 
 function doIt() {
 
-	sh ./brew.sh
-	sh ./macos.sh
+    sh ./brew.sh
+    sh ./macos.sh
 
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude "bootstrap.sh" \
-		--exclude "macos.sh" \
-		--exclude "brew.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE" \
-		-avh --no-perms . ~;
-	source ~/.zshrc;
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "bootstrap.sh" \
+        --exclude "macos.sh" \
+        --exclude "brew.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE" \
+        -avh --no-perms . ~;
+    source ~/.zshrc;
 
 
-	# Kill affected applications
-	for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-		"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-		"Photos" "Safari" "SystemUIServer" "iCal"; do
-		killall "${app}" &> /dev/null
-	done
-	echo "Done. Note that some of these changes require a logout/restart to take effect."
+    # Kill affected applications
+    for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+        "Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
+        "Photos" "Safari" "SystemUIServer" "iCal"; do
+        killall "${app}" &> /dev/null
+    done
+    echo "Done. Note that some of these changes require a logout/restart to take effect."
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
---
+++ sublime/README.md
@@ -0,0 +1,40 @@
+# Sublime
+
+
+## Theme
+[Afterglow](https://packagecontrol.io/packages/Theme%20-%20Afterglow)
+
+
+## Installed packages
+
+* AdvancedNewFile
+* Alignment
+* All Autocomplete
+* ApplySyntax
+* AutoFileName
+* BracketHighlighter
+* Color Highlighter
+* ColorPicker
+* Emmet
+* Emmet Css Snippets
+* Git
+* GitGutter
+* Handlebars
+* HTMLBeautify
+* JsFormat
+* LESS
+* LiveReload
+* Markdown Preview
+* Package Control
+* Sass
+* SASS Snippets
+* SCSS
+* Seti_UX
+* SFTP
+* SideBarEnhancements
+* SublimeCodeIntel
+* SublimeLinter
+* Terminal
+* Theme - Afterglow
+* TrailingSpaces
+
